e6212054856dca79939d06a3e3bcc9759ef63f05
MehrnazMirahmadi <125347723+MehrnazMirahmadi@users.noreply.github.com>
2024-09-03 13:18:18 +0330

    search

--- MashtiHasanResturant/wwwroot/js/site.js
+++ MashtiHasanResturant/wwwroot/js/site.js
@@ -168,25 +168,6 @@ $(document).on("click", ".btn-add", function () {
 
 });
 //*********************************************/
-//function DoSearch() {
-//    var action = $("#frmSearch").attr("data-action");
-//    var controller = $("#frmSearch").attr("data-controller");
-//    var formid = "#" + $().attr("data-form-id");
-//    var url = `/${controller}/${action}`;
-//    var data = $(formid).serialize();
-
-//    $.get(url, data, function (ListItem) {
-//        $("#DvContent").html(ListItem);
-//    })
-
-//}
-//document.on("change", ".drp", function () {
-//    DoSearch();
-//})
-
-//document.on("keyup", ".inp", function () {
-//    DoSearch();
-//})
 
 $(document).ready(function () {
     $("#CategoryId, #Name, #UnitPriceFrom, #UnitPriceTo").on("change keyup", function () {
